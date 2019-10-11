dac14d63d369eee6a854564f1d0901acfbc3f213
hs-yonekura <y-yonekura@science.co.jp>
2019-10-11 14:22:16 +0900

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  themeConfig: {
+    logo: 'https://www.science.co.jp/wordpress/wp-content/themes/humanscience/common/img/logo.jpg',
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: [
+      '/'
+    ],
+    displayAllHeaders: true,
+    sidebarDepth: 2
+  }
+}
